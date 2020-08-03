6da8fa09bc725eb590ac6ea4a7a983cdfbabfe1c
libao12345 <1349084222@qq.com>
2020-08-03 15:18:26 +0800

    create project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hi</div>
+  <Frank />
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
