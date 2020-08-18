51a0c117a697d279571940ab3d8e7398cc4bac83
libao12345 <1349084222@qq.com>
2020-08-18 10:53:07 +0800

    初始化aside的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-        <Topnav/>
+        <Topnav />
         <div class="banner">
             <h1>轱辘UI</h1>
             <h2>一个厉害的 UI 框架</h2>
             <p class="actions">
-                <a href="">Github</a>
-                <a href="">开始</a>
+                <a href="https://github.com/libao12345/gulu-ui-1">Github</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue"
 export default {
     components: {
         Topnav
@@ -22,5 +22,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
