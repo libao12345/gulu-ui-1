0a1897d9f375e5b989052c1c2797f179b238e5b4
libao12345 <1349084222@qq.com>
2020-08-18 10:31:25 +0800

    Topnav  封装

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import './index.scss'
 import {createWebHashHistory, createRouter} from "vue-router"
-import Frank from './components/Frank.vue'
-import Frank2 from './components/Frank2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const histiry = createWebHashHistory()
 const router = createRouter({
     history: histiry,
     routes: [
-        {path: '/', component: Frank},
-        {path: '/xxx', component: Frank2}
+        {path: '/', component: Home},
+        {path: '/doc', component: Doc}
     ]
 })
 
