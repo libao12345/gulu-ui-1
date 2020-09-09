af44fc4c8bf96a65bfb04932b12e36237a4d3e8a
libao12345 <1349084222@qq.com>
2020-09-09 12:03:26 +0800

    优化代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -37,16 +37,22 @@ export default {
         width: $h2;
         background:white;
         border-radius: $h2 / 2;
-        transition: left 250ms;
+        transition: all 250ms;
     } 
     button.checked {
-        background: blue;
+        background: #1890ff;
     }
     button.checked > span {
         left: calc(100% - #{$h2} - 2px);
     }
     button:focus {
         outline: none;
+    }
+    button:active{
+        > span {width: $h2 + 4px;}
+    }
+    button.checked:active{
+        > span {width: $h2 + 4px; margin-left: -4px;}
     }
 </style>
 
