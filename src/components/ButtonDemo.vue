f3958af3a43ca0e14bdc47d422e5e1010d27076e
libao12345 <1349084222@qq.com>
2020-10-10 16:48:23 +0800

    Button事件绑定

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,14 +4,24 @@
     </div>
     <h1>s示例1</h1>
     <div>
-        <Button>你好</Button>
+        <Button @click="onClick"
+            @focus="onClick"
+            @mouseover="onClick"
+            size="small"
+        >你好</Button>
     </div>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue"
 export default {
-    components: {Button}
+    components: {Button},
+    setup() {
+        const onClick = () => {
+            console.log("hello")
+        }
+        return {onClick}
+    } 
 }
 </script>
 
