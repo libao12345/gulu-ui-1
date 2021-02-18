0488b38148d1a71a15512c467b485da750a56516
libao12345 <1349084222@qq.com>
2021-02-18 10:51:05 +0800

    CSS最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="toggle" :class="{checked: value}"><span></span>
+    <button class="gulu-switch" @click="toggle" :class="{'gulu-checked': value}"><span></span>
     </button>
 </template>
 
@@ -21,7 +21,7 @@ export default {
 <style lang="scss" >
     $h: 22px;
     $h2: $h - 4px;
-    button{
+    .gulu-switch{
         height: $h;
         width: $h*2;
         border: none;
@@ -38,7 +38,7 @@ export default {
             border-radius: $h2 / 2;
             transition: all 250ms;
         }
-        &.checked {
+        &.gulu-checked {
             background: #1890ff;
             > span {
                 left: calc(100% - #{$h2} - 2px);
@@ -50,7 +50,7 @@ export default {
         &:active{
             > span {width: $h2 + 4px;}
         }
-        &.checked:active{
+        &.gulu-checked:active{
             > span {width: $h2 + 4px; margin-left: -4px;}
         } 
     }
