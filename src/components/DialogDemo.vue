1c708f12ad9efa79997779d6c94890b67b1a04ff
libao12345 <1349084222@qq.com>
2021-02-18 16:11:43 +0800

    一句代码打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -13,12 +13,15 @@
             <strong>加粗的标题</strong>
         </template>
     </Dialog>
+    <h1>示例2</h1>
+    <Button @click="showDialog">toggle</Button>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue"
 import Dialog from "../lib/Dialog.vue"
 import { ref } from "vue"
+import { openDialog } from "../lib/openDialog"
 export default {
     components: { 
         Dialog,
@@ -34,11 +37,24 @@ export default {
         }
         const f2 = () => {
         }
+        const showDialog = () => {
+            openDialog({
+                title: "标题",
+                content: "你好",
+                ok() {
+                    console.log('ok')
+                },
+                cancel() {
+                    console.log('cancel')
+                }     
+            })
+        }
         return {
             x,
             toggle,
             f1,
-            f2
+            f2,
+            showDialog
         }
     }
 }
