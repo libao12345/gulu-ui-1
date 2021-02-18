d1a6ede2a4547e5594181bcfb4755355295d668b
libao12345 <1349084222@qq.com>
2021-02-18 13:22:23 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,6 +2,15 @@
     <div>
         Dialog 的示例
     </div>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue"
+export default {
+    components: { Dialog },
+}
+</script>
 
 
