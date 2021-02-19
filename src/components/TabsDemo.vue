c99c49aa7bb036263fece661633fb913b08a8e31
libao12345 <1349084222@qq.com>
2021-02-19 15:19:20 +0800

    遇到Vue3 bug

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,20 +1,26 @@
 <template>
-    <div> Tabs 的示例 </div>
-    <h1>示例1</h1>
-    <Tabs selected="导航1">
-        <Tab title="导航1">内容1</Tab>
-        <Tab title="导航2">内容2</Tab>
-    </Tabs>
+  <div>Tabs 的示例</div>
+  <h1>示例1</h1>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 <script>
-import Tabs from "../lib/Tabs.vue"
-import Tab from "../lib/Tab.vue"
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
 
 export default {
-    components: {
-        Tabs, Tab
-    }
-}
+  components: {
+    Tabs,
+    Tab
+  },
+  setup() {
+    const x = ref("导航1")
+    return { x }
+  }
+};
 </script>
 
 
