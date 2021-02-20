6dae4fc5c8938d6dec7521932b72a2ad2d1b9509
libao12345 <1349084222@qq.com>
2021-02-20 17:28:44 +0800

    解决bug

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,18 @@
 import {createWebHashHistory, createRouter} from "vue-router"
 import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import DocDemo from './components/DocDemo.vue'
+import Doc from './views/Doc.vue';
 import SwitchDome from './components/SwitchDome.vue'
 import Buttondemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStart from './views/GetStart.vue'
-import Install from './views/Install.vue'
+import intro from './markdown/intro.md'
+import getStart from './markdown/get-started.md'
+import install from './markdown/install.md'
 import Markdown from './components/Markdown.vue'
 import { h } from "vue"
 
 const histiry = createWebHashHistory()
-const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename})
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
     history: histiry,
     routes: [
@@ -23,9 +22,9 @@ export const router = createRouter({
             component: Doc , 
             children:[
                 {path: '', redirect: '/doc/intro'},
-                {path: 'intro', component: md('intro')},
-                {path: 'get-start', component: md('get-started')},
-                {path: 'install', component: md('install')},
+                {path: 'intro', component: md(intro)},
+                {path: 'get-start', component: md(getStart)},
+                {path: 'install', component: md(install)},
                 {path: 'switch', component: SwitchDome},
                 {path: 'button', component: Buttondemo},
                 {path: 'dialog', component: DialogDemo},
